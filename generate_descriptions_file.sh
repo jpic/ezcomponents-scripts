21504daa842701a3d969b8ff0c7bc556ba0a7969
dr <dr@bc0e7bdc-f0fc-0310-8ff6-f601c06e1256>
2005-11-17 14:42:02 +0000

    - Fixed typoes in descriptions - Added a script that creates a file with all the components' descriptions.
    git-svn-id: http://svn.ez.no/svn/ezcomponents/scripts@989 bc0e7bdc-f0fc-0310-8ff6-f601c06e1256

---
+++ generate_descriptions_file.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# This script reads all the DESCRIPTION files in all package directories, and
+# puts this output into the docs/components_descriptions_marketing.txt file.
+
+exec > docs/components_descriptions_marketing.txt
+
+for i in packages/*; do
+	packagename=`echo $i | sed 's/packages\///'`
+	if test $packagename == 'autoload'; then
+		continue;
+	fi
+	if test -f $i/trunk/DESCRIPTION; then
+		echo $packagename;
+
+		cat $i/trunk/DESCRIPTION
+		echo
+	fi
+done
