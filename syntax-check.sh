b493a0128ff3006d66743aee2a1a4bf4499da2b0
dr <dr@bc0e7bdc-f0fc-0310-8ff6-f601c06e1256>
2005-11-21 20:01:52 +0000

    - Update syntax checking script #- This script *will* change some code on its own, so be careful!
    git-svn-id: http://svn.ez.no/svn/ezcomponents/scripts@1057 bc0e7bdc-f0fc-0310-8ff6-f601c06e1256

--- syntax-check.sh
+++ syntax-check.sh
@@ -1,3 +1,37 @@
 #!/bin/sh
 
 for i in `find . -name \*.php`; do php -l $i | grep -v "No syntax errors"; done
+
+for i in `find . -name \*.php`; do
+	cat $i | \
+		sed -e 's/[[:space:]]while(/ while (/' | \
+		sed -e 's/[[:space:]]if(/ if (/' | \
+		sed -e 's/[[:space:]]catch(/ catch (/' | \
+		sed -e 's/[[:space:]]foreach(/ foreach (/' | \
+		sed -e 's/[[:space:]]switch(/ switch (/' > /tmp/temporary.php
+	cp /tmp/temporary.php $i
+done
+
+echo "Checking for wrong braces placement for functions"
+grep -rn "function" * | grep "{" | grep -v svn | grep "\.php:"
+grep -rn "class" * | grep "{" | grep -v svn | grep "\.php:"
+grep -rn "interface" * | grep "{" | grep -v svn | grep "\.php:"
+
+echo "Checking for wrong if/else + brackets"
+grep -rn "if" * | grep "{" | grep -v svn | grep "\.php:"
+grep -rn "else" * | grep "{" | grep -v svn | grep "\.php:"
+
+
+echo "Checking for wrong 'try' syntax':"
+grep -nr "try" * | grep "[}{]" | grep -v svn-base | grep "\.php"
+
+echo "Checking for wrong 'catch' syntax':"
+grep -nr "catch" * | grep "[}{]" | grep -v svn-base | grep "\.php"
+
+echo "Checking for wrong closing bracket:"
+grep -nr "[^[:space:](]);" * | grep -v svn-base | grep -v tests | grep "\.php"
+
+echo "Checking for wrong opening bracket:"
+grep -nr "([^[:space:]C)]" * | grep -v svn-base | grep -v tests | grep -v "(string)" | grep -v "(int)" | grep -v "(float)" | grep -v "*" | grep "\.php:"
+
+for i in `find . -name \*.php`; do php -l $i | grep -v "No syntax errors"; done
