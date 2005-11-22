b89fa6229df1d932076e6cb81e1dc817f843e670
dr <dr@bc0e7bdc-f0fc-0310-8ff6-f601c06e1256>
2005-11-22 10:03:29 +0000

    - Added a script to replace the autogen tags with the correct version number.   You should run this from *within* the packages/<packagename>/releases/<version>   directory.
    git-svn-id: http://svn.ez.no/svn/ezcomponents/scripts@1065 bc0e7bdc-f0fc-0310-8ff6-f601c06e1256

---
+++ autogen-version-tags.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Usage:
+#   You should run this from *within* the
+#   packages/<packagename>/releases/<version> directory.
+
+version=`pwd | sed 's/.*\///g'`
+
+echo $version
+
+for i in `find . -name \*.php`; do perl -p -i -e "s/\/\/autogentag\/\//$version/g" $i; done
+for i in `find . -name \*.php`; do perl -p -i -e "s/\/\/autogen\/\//$version/g" $i; done
