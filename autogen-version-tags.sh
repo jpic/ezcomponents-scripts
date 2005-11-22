6fc19756af2fd6bf58d21325573894194ffcb0fe
dr <dr@bc0e7bdc-f0fc-0310-8ff6-f601c06e1256>
2005-11-22 13:45:08 +0000

    - Changed license to BSD.
    git-svn-id: http://svn.ez.no/svn/ezcomponents/scripts@1075 bc0e7bdc-f0fc-0310-8ff6-f601c06e1256

--- autogen-version-tags.sh
+++ autogen-version-tags.sh
@@ -10,3 +10,6 @@ echo $version
 
 for i in `find . -name \*.php`; do perl -p -i -e "s/\/\/autogentag\/\//$version/g" $i; done
 for i in `find . -name \*.php`; do perl -p -i -e "s/\/\/autogen\/\//$version/g" $i; done
+
+date=`date +"%A %d %B %Y"`
+perl -p -i -e "s/\[RELEASEDATE\]/$date/" ChangeLog
