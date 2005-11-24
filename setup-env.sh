b609583a6a41b9ce24697102877ddf75e108a126
dr <dr@bc0e7bdc-f0fc-0310-8ff6-f601c06e1256>
2005-11-24 12:36:25 +0000

    - Move script out of Base into the script directory where it belongs
    git-svn-id: http://svn.ez.no/svn/ezcomponents/scripts@1156 bc0e7bdc-f0fc-0310-8ff6-f601c06e1256

---
+++ setup-env.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if test -d autoload; then
+	echo "Autoload directory exists."
+else
+	echo "Creating missing 'autoload' directory."
+	mkdir autoload
+fi
+
+for i in `find . -name \*_autoload.php`; do
+	p=`echo $i | cut -d / -f 2`;
+	r=`echo $i | cut -d / -f 3`;
+
+	if test ! $p == "autoload"; then
+		if test ! $r == "releases"; then
+			b=`echo $i | cut -d / -f 5`
+			if test -L autoload/$b; then
+				echo "Symlink for $b to $i exists."
+			else
+				echo "Creating symlink from $i to autoload/$b."
+				ln -s "../$i" "autoload/$b"
+			fi
+		fi
+	fi
+done
