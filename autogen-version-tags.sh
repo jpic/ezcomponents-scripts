c694cdee3c22a826a564d8ecad068a26ce2ceef3
dr <dr@bc0e7bdc-f0fc-0310-8ff6-f601c06e1256>
2005-12-21 09:49:59 +0000

    - Changed version script to also modify the release date in trunk/ChangeLog
    git-svn-id: http://svn.ez.no/svn/ezcomponents/scripts@1556 bc0e7bdc-f0fc-0310-8ff6-f601c06e1256

--- autogen-version-tags.sh
+++ autogen-version-tags.sh
@@ -12,4 +12,4 @@ for i in `find . -name \*.php`; do perl -p -i -e "s/\/\/autogentag\/\//$version/
 for i in `find . -name \*.php`; do perl -p -i -e "s/\/\/autogen\/\//$version/g" $i; done
 
 date=`date +"%A %d %B %Y"`
-perl -p -i -e "s/\[RELEASEDATE\]/$date/" ChangeLog
+perl -p -i -e "s/$version\ \-\ \[RELEASEDATE\]/$version - $date/" ChangeLog ../../trunk/ChangeLog
