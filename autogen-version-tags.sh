30129c3c9bf5b5cc48d6857be731f61288e5063f
dr <dr@bc0e7bdc-f0fc-0310-8ff6-f601c06e1256>
2006-10-04 14:59:14 +0000

    - Updated release scripts.
    git-svn-id: http://svn.ez.no/svn/ezcomponents/scripts@3646 bc0e7bdc-f0fc-0310-8ff6-f601c06e1256

--- autogen-version-tags.sh
+++ autogen-version-tags.sh
@@ -4,13 +4,15 @@
 #   You should run this from *within* the
 #   releases/<packagename>/<version> directory.
 
-version=`pwd | sed 's/.*\///g'`
+fullVersion=`pwd | sed 's/.*\///g'`
+minorVersion=`pwd | sed 's/.*\///g' | sed 's/^\([0-9]\+\.[0-9]\+\).*/\1/'`
 comp=`pwd | cut -d / -f 6`
 
-echo $version
-
-for i in `find . -name \*.php`; do perl -p -i -e "s/\/\/autogentag\/\//$version/g" $i; done
-for i in `find . -name \*.php`; do perl -p -i -e "s/\/\/autogen\/\//$version/g" $i; done
+for i in `find . -name \*.php`; do perl -p -i -e "s/\/\/autogentag\/\//$fullVersion/g" $i; done
+for i in `find . -name \*.php`; do perl -p -i -e "s/\/\/autogen\/\//$fullVersion/g" $i; done
 
 date=`date +"%A %d %B %Y"`
-perl -p -i -e "s/$version\ \-\ \[RELEASEDATE\]/$version - $date/" ChangeLog ../../../trunk/$comp/ChangeLog
+perl -p -i -e "s/$fullVersion\ \-\ \[RELEASEDATE\]/$fullVersion - $date/" \
+	ChangeLog \
+	../../../trunk/$comp/ChangeLog \
+	../../../stable/$comp/$minorVersion/ChangeLog
--- release2.sh
+++ release2.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
 
 if test $# != 3; then
-	echo "Usage: ./scripts/release1.sh [component] [baseversion] [version]";
+	echo "Usage: ./scripts/release2.sh [component] [baseversion] [version]";
 	exit;
 fi
 
-component=$1
+componentRaw=$1
 baseversion=$2
 version=$3
 echo
 
+echo -n "* Figuring out whether this is stable or trunk: "
+component=`echo $componentRaw | cut -d / -f 1`
+componentRelease=`echo $componentRaw | cut -d / -f 2`
+if test $component == $componentRelease; then
+	echo "trunk"
+	branch=trunk
+else
+	echo "stable/$componentRelease"
+	branch=stable
+fi
+
 echo "* Copying to release branch"
-svn cp trunk/$component releases/$component/$version
+if test $branch == 'trunk'; then
+	svn cp trunk/$component releases/$component/$version
+else
+	svn cp stable/$component/$componentRelease releases/$component/$version
+fi
+
 cd releases/$component/$version
 ../../../scripts/autogen-version-tags.sh
 cd ../../..
@@ -21,7 +37,11 @@ cat release-info/latest | sed "s/$component:.*/$component: $version/" > /tmp/rel
 mv /tmp/release-info release-info/latest
 
 echo "* Committing component to SVN"
-svn commit -m "- Released $component version $version" trunk/$component releases/$component/$version release-info/latest
+if test $branch == 'trunk'; then
+	echo svn commit -m "- Released $component version $version" trunk/$component releases/$component/$version release-info/latest
+else
+	echo svn commit -m "- Released $component($componentRelease) version $version" trunk/$component stable/$component/$componentRelease releases/$component/$version release-info/latest
+fi
 
 echo "* Creating PEAR package"
 scripts/create_pear_package.php -v $version -b $baseversion -p $component
