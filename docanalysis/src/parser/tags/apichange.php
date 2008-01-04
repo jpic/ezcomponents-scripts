05f84b03a64ec902ff8edee4bd2174a949834116
ts <ts@bc0e7bdc-f0fc-0310-8ff6-f601c06e1256>
2008-01-04 13:13:34 +0000

    - Adjusted @apichange tag. - Fixed issue #10928: Add support for more tags like @apichange.
    git-svn-id: http://svn.ez.no/svn/ezcomponents/scripts@7071 bc0e7bdc-f0fc-0310-8ff6-f601c06e1256

--- docanalysis/src/parser/tags/apichange.php
+++ docanalysis/src/parser/tags/apichange.php
@@ -4,18 +4,18 @@ class ezcDocBlockApichangeTag extends ezcDocBlockBaseTag implements ezcDocBlockT
 {
     public static function getPattern()
     {
-        return '/^@apichange(\s|$)/';
+        return '/^@apichange(\s*|$)/';
     }
 
     public function __construct( $docLine )
     {
-        if ( preg_match( '/^@apichange\s*(\S+\s*)*$/', $docLine, $matches ) !== 1 )
+        if ( preg_match( '/^@apichange\s*(\S.*)$/', $docLine, $matches ) !== 1 )
         {
             throw new ezcDocInvalidDocTagException( "apichange", $docLine );
         }
         parent::__construct(
             array(
-                "ref"  => ( isset( $matches[1] ) ? $matches[1] : 'none' ),
+                'desc' => ( isset( $matches[1] ) ? $matches[1] : "" )
             )
         );
     }
