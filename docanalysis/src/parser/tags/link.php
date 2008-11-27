b1bf4e7b005cf408a80e5fc920e972519bc87ebd
ts <ts@bc0e7bdc-f0fc-0310-8ff6-f601c06e1256>
2008-11-27 12:08:24 +0000

    - Fix @link tag parser to allow link description.
    git-svn-id: http://svn.ez.no/svn/ezcomponents/scripts@9463 bc0e7bdc-f0fc-0310-8ff6-f601c06e1256

--- docanalysis/src/parser/tags/link.php
+++ docanalysis/src/parser/tags/link.php
@@ -9,13 +9,14 @@ class ezcDocBlockLinkTag extends ezcDocBlockBaseTag implements ezcDocBlockTag
 
     public function __construct( $docLine )
     {
-        if ( preg_match( '/^@link\s+(\S+)\s+$/', $docLine, $matches ) !== 1 )
+        if ( preg_match( '/^@link\s+(\S+)\s+(.*)$/', $docLine, $matches ) !== 1 )
         {
             throw new ezcDocInvalidDocTagException( "link", $docLine );
         }
         parent::__construct(
             array(
                 "ref"  => $matches[1],
+                "text" => $matches[2],
             )
         );
     }
