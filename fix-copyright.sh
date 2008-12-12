40d78d87ac405a83c13b779d35b8d7e332c98b24
dr <dr@bc0e7bdc-f0fc-0310-8ff6-f601c06e1256>
2008-12-12 13:11:34 +0000

    - Make sure to use the correct speling of eZ Systems upon the next run.
    git-svn-id: http://svn.ez.no/svn/ezcomponents/scripts@9589 bc0e7bdc-f0fc-0310-8ff6-f601c06e1256

--- fix-copyright.sh
+++ fix-copyright.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-for i in `find . -name \*.php`; do perl -p -i -e "s/^\s\*\s\@copyright(.*)/ * \@copyright Copyright (C) 2005-2008 eZ systems as. All rights reserved./g" $i; done
+for i in `find . -name \*.php`; do perl -p -i -e "s/^\s\*\s\@copyright(.*)/ * \@copyright Copyright (C) 2005-2008 eZ Systems AS. All rights reserved./g" $i; done
